3af5748f5b766c528a9f738c9bcd932726380bd6
bukit3point0 <bukit3point0@gmail.com>
2021-03-10 01:53:03 -0500

    created a useLocalStorage dark mode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
+
+import useDarkMode from './components/hooks/useDarkMode'
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
@@ -8,17 +9,25 @@ import Navbar from "./components/Navbar";
 import "./styles.scss";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
+  const [
+    coinData,
+    darkMode,
+    setDarkMode
+  ] = useDarkMode()
+
+  // const [coinData, setCoinData] = useState([]);
+  // const [darkMode, setDarkMode] = useState(false);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+  //     )
+  //     .then(res => setCoinData(res.data))
+  //     .catch(err => console.log(err));
+  // }, []);
+
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
