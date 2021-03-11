3ee4e9c38100a83d24b9512b001e885194cd3b3b
cynthicrush <cynthiazcanada@gmail.com>
2021-03-10 22:20:24 -0500

    Homework changes

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,10 @@
+import useLocalStorage from './useLocalStorage';
+
+
+const useDarkMode = (key) => {
+    const [darkMode, setDarkMode] = useLocalStorage(key)
+
+    return [darkMode, setDarkMode];
+}
+
+export default useDarkMode
