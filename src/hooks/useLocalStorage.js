f8f3ee6052c066ba5b9fbaefcefa607ffd5ca032
Matthew Larson <maflarson@gmail.com>
2021-03-20 14:19:19 -0600

    asdf

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,26 +1,19 @@
-import React, {useState} from 'react';
+import {useState} from "react"
 
-const useLocalStorage = (key, initialValue) => {
-    //1. when initializing our state, see if our data exists in localStorage.
-    //2. If data exists on localStorage, make that our initialState value.
-    //3. If data does not exist on localStorage, make our state = initialState and save initialState to localStorage.
-    //4. When setting data, save data to state AND save data to localstorage/
+//const useLocalStorage = (key, initialValue) => {
+    //const [storedValue, setStoredValue] = useState(() => {
+    const useLocalStorage = (key, initialValue) => {
+        const[storedValue, setStoredValue] = useState(() =>{
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        })
+    
 
-    const [storedState, setStoredState] = useState(()=> {
-        if (localStorage.getItem(key)) {
-            return JSON.parse(localStorage.getItem(key));
-        } else {
-            localStorage.setItem(key, JSON.stringify(initialValue));
-            return initialValue;
-        }
-    });
-
-    const setState = value => {
-        setStoredState(value);
-        localStorage.setItem(key, JSON.stringify(value));
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value))
     }
-
-    return([storedState, setState]);
+    return[storedValue, setValue]
 }
 
-export default useLocalStorage;
+export default useLocalStorage
