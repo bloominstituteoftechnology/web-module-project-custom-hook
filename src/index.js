6e22e616dbba8d3100e1e00387bb0332d5e7bdea
Ricky Klusmeier <rickyklusmeier@Rickys-MacBook-Pro-2.local>
2021-04-07 13:48:16 -0600

    MVP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
-
+import DarkMode from './components/hooks/DarkMode'
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
@@ -9,17 +8,7 @@ import Navbar from "./components/Navbar";
 import "./styles.scss";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
+  const [ coinData, darkMode, setDarkMode ] = DarkMode()
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
