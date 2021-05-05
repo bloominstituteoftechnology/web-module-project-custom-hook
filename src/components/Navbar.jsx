b032662a01d041d87f44f4fb8fd126d8b27bfc32
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-05-05 16:46:32 -0400

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Navbar = (props) => {
-  const toggleMode = e => {
+  const toggleMode = (e) => {
     e.preventDefault();
     props.setDarkMode(!props.darkMode);
   };
 
   return (
     <nav className="navbar">
-      <h1>Crypto Tracker</h1>
+      <h1>Kaz's Crypto Tracker</h1>
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
-          className={props.darkMode ? 'toggle toggled' : 'toggle'}
+          className={props.darkMode ? "toggle toggled" : "toggle"}
         />
       </div>
     </nav>
