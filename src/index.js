c84f0a5ef78f2cb39ace67d68a424dda0d68cbf1
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-05-11 21:31:00 -0400

    mvp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-
+import useDarkMode from './hooks/useDarkMode';
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
-
-
 import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
-
+  const [darkMode, setDarkMode] = useDarkMode(false);
   useEffect(() => {
     axios
       .get(
@@ -25,8 +22,7 @@ const App = () => {
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <Charts coinData={coinData} />
     </div>
-  );
+);
 };
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
