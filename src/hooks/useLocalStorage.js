95ff639076e2d0e5081e82f344d9f99749d1087d
John Laubscher <jlmemphiscommanders@gmail.com>
2021-06-02 22:04:00 -0500

    done with mvp

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
+    if (localStorage.getItem(key)) {
+      return JSON.parse(localStorage.getItem(key));
+    }
+    localStorage.setItem(key, JSON.stringify(initialValue));
+    return initialValue;
+  });
+  const setLocalStoredValue = (value) => {
+    setValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+  return [value, setLocalStoredValue];
+};
+
+export default useLocalStorage;
