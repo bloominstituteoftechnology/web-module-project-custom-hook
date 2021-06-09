4dd8b7d3007ef4be9834513d46f918ebd10824ee
Joseph Esquivel <josephesquivel@Josephs-MacBook-Air.local>
2021-06-08 17:27:27 -0700

    mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const darkModeOn = "false";
+
+const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useLocalStorage(darkModeOn);
+    return [darkMode, setDarkMode];
+};
+
+export default useDarkMode;
