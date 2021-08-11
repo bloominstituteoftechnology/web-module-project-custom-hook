86afdd97d2324c3a057170f761dddcb23985f20a
Owen  Damron <damronowen@yahoo.com>
2021-08-10 23:29:28 -0500

    Finished MVP

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {useState} from "react"
+
+const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+       if (window.localStorage.getItem(key)) {
+            return JSON.parse(window.localStorage.getItem(key))
+       }
+       else {
+            return initialValue;
+       }   
+    });
+
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    return [storedValue, setValue];
+}
+
+ export default useLocalStorage;
