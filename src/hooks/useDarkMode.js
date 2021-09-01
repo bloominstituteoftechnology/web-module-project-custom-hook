91c00d849e83eed2b497b8191557469e21eff184
courtneyharris <harriscourtney98@gmail.com>
2021-09-01 18:21:46 -0400

    This was nice

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,19 @@
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = (key, initialValue) => {
+    const [darkMode, setDarkMode ] = useLocalStorage (() => {
+
+        const item = window.localStorage.getItem(key);
+        return item? JSON.parse(item) : JSON.stringify
+        (initialValue)
+    })
+
+    const setDark = (value) => {
+        window.localStorage.setItem(key, JSON.stringify 
+            (value))
+            setDarkMode(value)
+    }
+        return [darkMode, setDark]
+}
+
+export default useDarkMode;
