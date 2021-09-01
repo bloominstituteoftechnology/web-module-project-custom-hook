ba86f673c936e4abdfa07f6853e2a3a2cb068a04
Leo Hays <starryhornet@gmail.com>
2021-09-01 16:18:05 -0500

    Darkmode memory module complete

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,10 @@
+import  useLocalStorage  from "./useLocalStorage"
+
+
+const useDarkMode = (initialValue) => {
+  const [value, setValue] = useLocalStorage('screen-mode', initialValue);
+
+  return ([value, setValue])
+}
+
+export default useDarkMode;
