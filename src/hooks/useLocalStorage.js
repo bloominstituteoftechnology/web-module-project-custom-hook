54b1ccf70596f2753815e2772f8ea81473cd7fb0
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-09-01 13:52:00 -0600

    Added useLocalStorage function

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,1 +1,19 @@
+import { useState } from 'react';
 
+const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(()=> {
+        const item = localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    });
+
+    const setValue = (value) => {
+        localStorage.setItem(key, JSON.stringify(value));
+        setStoredValue(value);
+    }
+
+
+    return [storedValue, setValue];
+};
+
+
+export default useLocalStorage;
