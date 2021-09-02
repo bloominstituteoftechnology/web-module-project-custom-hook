ffedbc8154986f0ca581a9771ded094b3fcd2e0c
Sebastian <sebas6292@gmail.com>
2021-09-01 17:01:53 -0700

    i think this is right

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,9 @@
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = (initialValue) => {
+    const [value, setValue] = useLocalStorage('screen-mode',initialValue);
+
+    return ([value, setValue])
+}
+
+export default useDarkMode;
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,21 @@
+import { useState } from 'react'; 
+
+const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(()=> {
+        const item = window.localStorage.getItem(key);
+        if(item) {
+            return JSON.parse(item)
+        } else {
+            localStorage.setItem(key,JSON.stringify(initialValue))
+            return (initialValue)
+        }
+    });
+    const setValue = (value) => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.Stringify(value));
+    };
+
+    return [storedValue, setValue];
+};
+
+export default useLocalStorage;
