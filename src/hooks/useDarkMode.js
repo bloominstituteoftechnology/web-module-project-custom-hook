968d163e3767026dde6166a1370d392cb1555e3e
Sebastian <sebas6292@gmail.com>
2021-09-01 17:07:10 -0700

    additional stuff

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,7 @@
 import useLocalStorage from "./useLocalStorage";
 
 const useDarkMode = (initialValue) => {
-    const [value, setValue] = useLocalStorage('screen-mode',initialValue);
+    const [value, setValue] = useLocalStorage('screen-mode', initialValue);
 
     return ([value, setValue])
 }
