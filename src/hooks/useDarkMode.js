eb769853342a688942ef08d2b674e2f9e3a5dd06
NeNeTheCreative <shecodes@nenethecreative.com>
2021-09-01 21:20:32 -0400

    code fixed.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,13 +1,16 @@
 import  useLocalStorage  from './useLocalStorage';
 
 const useDarkMode = (key, initialValue) => {
-    const [darkMode, setDarkMode] = useLocalStorage(() => {
-        const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : JSON.stringify(initialValue)
-    })
+    // const [darkMode, setDarkMode] = useLocalStorage(() => {
+    //     const item = window.localStorage.getItem(key);
+    //     return item ? JSON.parse(item) : JSON.stringify(initialValue)
+    // })
+    
+    const [darkMode,setDarkMode ] = useLocalStorage(key, initialValue) 
 
+    
     const setDark = (value) => {
-        window.localStorage.setItem(key, JSON.stringify(value))
+        // window.localStorage.setItem(key, JSON.stringify(value))
         setDarkMode(value)
 
     }
