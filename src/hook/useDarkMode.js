a40ec509ad0e230545011be849e5e859ce1497a7
vida <ofo28@aol.com>
2021-09-07 22:28:05 -0400

    Hook files created

---
+++ src/hook/useDarkMode.js
@@ -0,0 +1,9 @@
+import { useLocalStorage } from "./useLocalStorage"
+	
+
+	export const useDarkMode = (param) => {
+	    const [darkMode, setDarkMode] = useLocalStorage(param, false)
+	
+
+	    return([darkMode, setDarkMode])
+	}
---
+++ src/hook/useLocalStorage.js
@@ -0,0 +1,25 @@
+import {useState} from 'react'
+
+import "./styles.scss";
+
+
+	export const useLocalStorage = (key, initialValue) => {
+	    
+	    const [value, setValue] = useState(() => {
+	      if (localStorage.getItem(key)) {
+	        return (JSON.parse(localStorage.getItem(key)));
+	      } else {
+	        localStorage.setItem(key, JSON.stringify(initialValue));
+	        return initialValue;
+	      }
+	    });
+	
+
+	    const setStoredValue = (value) => {
+			
+	      localStorage.setItem(key, JSON.stringify(value));
+	      setValue(value);
+	    }
+	  
+	    return ([value, setStoredValue]);
+	  }
