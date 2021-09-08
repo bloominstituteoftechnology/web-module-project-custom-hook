17bdf7418c691241287cf87df20187c842cfff8d
markporo <markporo@yahoo.com>
2021-09-07 23:30:22 -0400

    dark mode project..using custom hook successfully

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,9 @@
+import useLocalStorage from "./useLocalStorage"
+
+const useDarkMode = (key) => {
+    const [darkMode, setDarkMode] = useLocalStorage(key)
+
+    return [darkMode, setDarkMode];
+}
+
+export default useDarkMode;
