08393fb2de2cc9e600581678e3a1e9fdea2da575
Angelique Abacajan <ma_utsey@yahoo.com>
2021-10-27 17:10:42 -0400

    Implemented useDarkMode.js

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,6 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useDarkMode = initialValue => {
+    const [value, setValue] = useLocalStorage("darkmode", initialValue)
+    return [value, setValue];
+}
