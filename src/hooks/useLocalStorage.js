3432c1b4369ffab49d0daecfe4dfc5a121517c85
Peter Conley <peterdavidconley@gmail.com>
2021-10-27 16:44:26 -0400

    used code from READme for the local storage function

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,22 +1,17 @@
 import React, { useState } from 'react'
 
-const useLocalStorage = (key, initialValue) => {
+export const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    });
 
-    const [value, setValue] = useState(()=> {
-        if (localStorage.getItem(key)) {
-          return JSON.parse(localStorage.getItem(key));
-        } else {
-          localStorage.setItem(key, JSON.stringify(initialValue));
-          return initialValue;
-        }
-      });
-    
-      const setStoredValue = (value)=> {
-        localStorage.setItem(key, JSON.stringify(value));
-        setValue(value);
-      }
-    
-      return [value, setStoredValue];
-}
-
-export default useLocalStorage
+    const setValue = value => {
+        // Save state
+        setStoredValue(value);
+        // Save to local storage
+        window.localStorage.setItem(key, JSON.stringify(value));
+      };
+  
+    return [storedValue, setValue];
+  };
