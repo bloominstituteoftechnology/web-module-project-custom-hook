d9cf2268f344454b3d9de38eaf15077caea4d950
cainpalmer <cain.a.palmer@gmail.com>
2021-10-27 16:14:08 -0400

    easy project

---
+++ src/components/hooks/useLocalStorage.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState (() => {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    });
+
+    const setValue = (value) => {
+        window.localStorage.setItem(key, value);
+        setStoredValue(value);
+    };
+
+    return [storedValue, setValue];
+}
+
+export {useLocalStorage};
