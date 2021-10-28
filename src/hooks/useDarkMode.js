a234bba7d2ab11366e1a4c3ca11f5fd96529eb30
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-10-27 21:42:48 -0400

    mvp

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,4 +1,4 @@
-import {useLocalStorage} from './useLocalStorage';
+import useLocalStorage from './useLocalStorage';
 
 const useDarkMode = (initialValue) => {
     const [value, setValue] = useLocalStorage('darkmode', initialValue);
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const useLocalStorage = (key , initialValue) =>{
-    const [storedValue, setStoredValue] = useState() (()=>{
+    const [storedValue, setStoredValue] = useState(()=>{
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;  
     })
@@ -12,4 +12,4 @@ const useLocalStorage = (key , initialValue) =>{
 
     return [storedValue, setValue];
 }
-export default useLocalStorage
+export default useLocalStorage;
