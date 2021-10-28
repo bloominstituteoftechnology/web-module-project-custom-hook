79c364a272e9fdfee9c881b6a7be5449a1d9d3cb
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-10-27 22:42:27 -0700

    STEP 3 - Using the hook in a component Pass

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import useLocalStorage from './hooks/useLocalStorage';
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import useDarkMode from './hooks/useDarkMode'
 
 
 import "./styles.scss";
@@ -11,7 +11,7 @@ import "./styles.scss";
 const App = () => {
   const [coinData, setCoinData] = useState([]);
   // const [darkMode, setDarkMode] = useState(false);
-  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
+  const [darkMode, setDarkMode] = useDarkMode("darkMode",false);
 
 
   useEffect(() => {
