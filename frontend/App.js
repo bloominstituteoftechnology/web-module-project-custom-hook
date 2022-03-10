ee8799a75d70410aa7412540bf82d9bdf939ccc6
Ladrillo <gabriel@cabrejas.net>
2022-03-10 14:50:11 +0100

    fix

--- frontend/App.js
+++ frontend/App.js
@@ -4,8 +4,6 @@ import axios from "axios";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
-import "./style.css";
-
 const App = () => {
   const [coinData, setCoinData] = useState([]);
   const [darkMode, setDarkMode] = useState(false);
